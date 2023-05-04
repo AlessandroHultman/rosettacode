3fcfd160453a0c03457f5df68ff891850868ca53
AlessandroHultman <alessandrosisfontes@hultman.com>
2023-05-04 09:54:06 +0200

    Fix some minor bugs.

--- rccli.py
+++ rccli.py
@@ -1,149 +1,84 @@
 import argparse
 import json
-import os
+from pathlib import Path
 from rosettacode import Category, Task
 
-parser = argparse.ArgumentParser(description="A CLI-tool for scraping source code")
-parser.add_argument("--task", help="The name of the task to scrape")
-parser.add_argument("--category", help="The name of the category to scrape")
-parser.add_argument("--languages", action="store_true", help="List the languages in the task or category")
-parser.add_argument("--count", action="store_true", help="Show the number of code blocks for each language")
-parser.add_argument("--language", help="The name of the language to filter by")
-parser.add_argument("--code", action="store_true", help="Show the code blocks for the task or category")
-parser.add_argument("--json", action="store_true", help="Dump JSON for the task or category data")
-parser.add_argument("--file", help="The name of the file to save the JSON data")
-parser.add_argument("--dir", help="The name of the directory to save the code blocks")
 
-args = parser.parse_args()
+def main():
+    """A command-line interface for scraping code from Rosetta Code."""
+    parser = argparse.ArgumentParser(
+        description="A command-line interface for scraping code from Rosetta Code."
+    )
+    parser.add_argument("--task", help="The name of the task to scrape")
+    parser.add_argument("--category", help="The name of the category to scrape")
+    parser.add_argument("--language", help="The name of the language to filter by")
+    parser.add_argument(
+        "--list",
+        action="store_true",
+        help="List the languages and their code blocks for the task or category",
+    )
+    parser.add_argument(
+        "--show",
+        action="store_true",
+        help="Show the code blocks for the task or category",
+    )
+    parser.add_argument(
+        "--json", action="store_true", help="Dump JSON for the task or category data"
+    )
+    parser.add_argument("--file", help="The name of the file to save the JSON data")
+    parser.add_argument(
+        "--dir", help="The name of the directory to save the code blocks"
+    )
+    args = parser.parse_args()
 
-if args.task:
-    # Scrape a task
-    task = Task(args.task)
-    if args.languages:
-        # List the languages in the task
-        for lang in task.languages:
-            if args.count:
-                # Show the number of code blocks for each language
-                print("{}: {}".format(lang.name, len(lang.blocks)))
-            else:
-                print(lang.name)
-    elif args.code:
-        # Show the code blocks for the task
-        if args.language:
-            # Filter by a specific language
-            lang = next((lang for lang in task.languages if lang.name == args.language), None)
-            if lang:
-                for block in lang.blocks:
-                    print(block.code)
-                    print()
-            else:
-                print("Language not found: {}".format(args.language))
-        else:
-            # Show all the code blocks for all languages
-            for lang in task.languages:
-                print(lang.name)
-                print()
-                for block in lang.blocks:
-                    print(block.code)
-                    print()
-    elif args.json:
-        # Dump JSON for the task data
-        data = task.to_json()
+    if args.task:
+        item = Task(args.task)
+    elif args.category:
+        item = Category(args.category)
+    else:
+        parser.error("You must specify either a task or a category")
+
+    if args.list:
+        print(f"Item: {item.name}")
+        for lang in item.languages:
+            if args.language and args.language not in lang.name:
+                continue  # skip this language if it does not match the filter
+            print(f"Language: {lang.name}")
+            print(f"Code blocks: {len(lang.blocks)}")
+
+    if args.show:
+        print(f"Item: {item.name}")
+        for lang in item.languages:
+            if args.language and args.language not in lang.name:
+                continue  # skip this language if it does not match the filter
+            print(f"Language: {lang.name}")
+            for block in lang.blocks:
+                print(f"Code length: {len(block.code)}")
+                print(block.code)
+
+    if args.json:
+        data = item.to_json()
         if args.file:
-            # Save the JSON data to a file
             with open(args.file, "w") as f:
-                json.dump(data, f, indent=4)
+                json.dump(data, f)
+            print(f"Saved JSON data to {args.file}")
         else:
-            # Print the JSON data to stdout
             print(json.dumps(data, indent=4))
-    elif args.dir:
-        # Save the code blocks to a directory
-        if not os.path.exists(args.dir):
-            os.makedirs(args.dir)
-        if args.language:
-            # Filter by a specific language
-            lang = next((lang for lang in task.languages if lang.name == args.language), None)
-            if lang:
-                for i, block in enumerate(lang.blocks):
-                    filename = "{}_{}_{}.txt".format(task.name, lang.name, i+1)
-                    filepath = os.path.join(args.dir, filename)
-                    with open(filepath, "w") as f:
-                        f.write(block.code)
-            else:
-                print("Language not found: {}".format(args.language))
-        else:
-            # Save all the code blocks for all languages
-            for lang in task.languages:
-                for i, block in enumerate(lang.blocks):
-                    filename = "{}_{}_{}.txt".format(task.name, lang.name, i+1)
-                    filepath = os.path.join(args.dir, filename)
-                    with open(filepath, "w") as f:
-                        f.write(block.code)
 
-elif args.category:
-    # Scrape a category
-    category = Category(args.category)
-    if args.tasks:
-        # List the tasks in the category
-        for task in category.tasks:
-            print(task.name)
-    elif args.code:
-        # Show the code blocks for the category
-        if args.language:
-            # Filter by a specific language
-            for task in category.tasks:
-                lang = next((lang for lang in task.languages if lang.name == args.language), None)
-                if lang:
-                    print(task.name)
-                    print()
-                    for block in lang.blocks:
-                        print(block.code)
-                        print()
-        else:
-            # Show all the code blocks for all languages
-            for task in category.tasks:
-                print(task.name)
-                print()
-                for lang in task.languages:
-                    print(lang.name)
-                    print()
-                    for block in lang.blocks:
-                        print(block.code)
-                        print()
-    elif args.json:
-        # Dump JSON for the category data
-        data = category.to_json()
-        if args.file:
-            # Save the JSON data to a file
-            with open(args.file, "w") as f:
-                json.dump(data, f, indent=4)
-        else:
-            # Print the JSON data to stdout
-            print(json.dumps(data, indent=4))
-    elif args.dir:
-        # Save the code blocks to a directory
-        if not os.path.exists(args.dir):
-            os.makedirs(args.dir)
-        if args.language:
-            # Filter by a specific language
-            for task in category.tasks:
-                lang = next((lang for lang in task.languages if lang.name == args.language), None)
-                if lang:
-                    for i, block in enumerate(lang.blocks):
-                        filename = "{}_{}_{}.txt".format(task.name, lang.name, i+1)
-                        filepath = os.path.join(args.dir, filename)
-                        with open(filepath, "w") as f:
-                            f.write(block.code)
-        else: 
-            # Save all the code blocks for all languages
-            for task in category.tasks: 
-                for lang in task.languages: 
-                    for i, block in enumerate(lang.blocks): 
-                        filename = "{}_{}_{}.txt".format(task.name, lang.name, i+1) 
-                        filepath = os.path.join(args.dir, filename) 
-                        with open(filepath, "w") as f: 
-                            f.write(block.code)
+    if args.dir:
+        output_dir = Path(args.dir)
+        output_dir.mkdir(exist_ok=True)
+        for lang in item.languages:
+            if args.language and args.language not in lang.name:
+                continue  # skip this language if it does not match the filter
+            for block in lang.blocks:
+                if block.code:
+                    file_name = f"{item.name}_{lang.name}.txt"
+                    file_path = output_dir / file_name
+                    with open(file_path, "w") as f:
+                        f.write(block.code)
+                    print(f"Saved code to {file_path}")
 
-else: 
-    # No arguments given 
-    parser.print_help()
+
+if __name__ == "__main__":
+    main()
--- rosettacode.py
+++ rosettacode.py
@@ -1,124 +1,158 @@
+import logging
 import requests
 from bs4 import BeautifulSoup
 
 
-BASE_URL = "https://rosettacode.org/wiki/"
-
-
 class Category:
+    """A class that represents a category page on Rosetta Code."""
+
     def __init__(self, name):
+        """Initialize the category with a name and a list of tasks."""
         self.name = name
-        self.url = BASE_URL + name
         self.tasks = []
         self._fetch_tasks()
 
     def _fetch_tasks(self):
-        response = requests.get(self.url)
-        soup = BeautifulSoup(response.text, "html.parser")
-        task_list = soup.find("div", id="mw-pages")
-        if task_list:
-            links = task_list.find_all("a")
-            for link in links:
-                task_name = link.text
-                task_url = link["href"]
-                self.tasks.append(Task(task_name, task_url))
+        """Fetch the tasks from the category page using requests and BeautifulSoup."""
+        url = f"https://rosettacode.org/wiki/Category:{self.name}"
+        logging.info(f"Fetching tasks from {url}")
+        response = requests.get(url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, "lxml")
+            task_list = soup.find("div", id="mw-pages")
+            if task_list:
+                task_links = task_list.find_all("a")
+                for link in task_links:
+                    task_name = link.text
+                    self.tasks.append(Task(task_name))
+            else:
+                logging.warning(f"No tasks found for category {self.name}")
+        else:
+            logging.error(f"Failed to fetch tasks from {url}: {response.status_code}")
 
     def to_json(self):
-        return {
-            "name": self.name,
-            "url": self.url,
-            "tasks": [task.to_json() for task in self.tasks],
-        }
+        """Return a JSON-serializable representation of the category."""
+        return {"name": self.name, "tasks": [task.to_json() for task in self.tasks]}
 
 
 class Task:
-    def __init__(self, name, url=None):
+    """A class that represents a task page on Rosetta Code."""
+
+    def __init__(self, name):
+        """Initialize the task with a name and a list of languages."""
         self.name = name
-        self.url = url or BASE_URL + name.replace(" ", "_")
         self.languages = []
         self._fetch_languages()
 
     def _fetch_languages(self):
-        response = requests.get(self.url)
-        soup = BeautifulSoup(response.text, "html.parser")
-        headings = soup.find_all("h2")
-        for heading in headings:
-            lang_name = heading.text.strip()
-            if lang_name.endswith("[edit]"):
-                lang_name = lang_name.replace("[edit]", "")
-                lang_codeblocks = []
-                next_sibling = heading.next_sibling
-                while next_sibling and next_sibling.name != "h2":
-                    if next_sibling.name == "pre":
-                        codeblock_text = next_sibling.text.strip()
-                        codeblock_output = None
-                        codeblock_metadata = None
-                        # Some code blocks have output or metadata in comments
-                        if codeblock_text.startswith("/*"):
-                            lines = codeblock_text.split("\n")
-                            comment_line = lines[0].strip()
-                            if comment_line.endswith("*/"):
-                                # Single line comment
-                                comment_text = comment_line[2:-2].strip()
-                                if comment_text.startswith("Output:"):
-                                    codeblock_output = comment_text.replace(
-                                        "Output:", ""
-                                    ).strip()
-                                elif comment_text.startswith("{"):
-                                    codeblock_metadata = comment_text
-                                codeblock_text = "\n".join(lines[1:]).strip()
-                            else:
-                                # Multi line comment
-                                comment_lines = []
-                                i = 0
-                                while i < len(lines) and not lines[i].strip().endswith(
-                                    "*/"
-                                ):
-                                    comment_lines.append(lines[i].strip())
-                                    i += 1
-                                comment_lines.append(lines[i].strip())
-                                comment_text = "\n".join(comment_lines)[2:-2].strip()
-                                if comment_text.startswith("Output:"):
-                                    codeblock_output = comment_text.replace(
-                                        "Output:", ""
-                                    ).strip()
-                                elif comment_text.startswith("{"):
-                                    codeblock_metadata = comment_text
-                                codeblock_text = "\n".join(lines[i + 1 :]).strip()
-                        lang_codeblocks.append(
-                            CodeBlock(
-                                codeblock_text, codeblock_output, codeblock_metadata
-                            )
-                        )
-                    next_sibling = next_sibling.next_sibling
-                print(lang_name, lang_codeblocks)
-                self.languages.append(Language(lang_name, lang_codeblocks))
+        """Fetch the languages from the task page using requests and BeautifulSoup."""
+        url = f"https://rosettacode.org/wiki/{self.name}"
+        logging.info(f"Fetching languages from {url}")
+        response = requests.get(url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, "lxml")
+            language_list = soup.find("div", id="toc")
+            if language_list:
+                language_links = language_list.find_all("a")[
+                    1:
+                ]  # skip the first link which is not a language
+                for link in language_links:
+                    language_name = link.text
+                    self.languages.append(Language(language_name, self.name))
+            else:
+                logging.warning(f"No languages found for task {self.name}")
+        else:
+            logging.error(
+                f"Failed to fetch languages from {url}: {response.status_code}"
+            )
 
     def to_json(self):
+        """Return a JSON-serializable representation of the task."""
         return {
             "name": self.name,
-            "url": self.url,
             "languages": [lang.to_json() for lang in self.languages],
         }
 
 
 class Language:
-    def __init__(self, name, blocks):
-        self.name = name
-        self.blocks = blocks
+    """A class that represents a language section on a task page on Rosetta Code."""
 
-    def __repr__(self) -> str:
-        return str(len(self.blocks))
+    def __init__(self, name, task_name):
+        """Initialize the language with a name, a task name, and a list of code blocks."""
+        self.name = name
+        self.task_name = task_name
+        self.blocks = []
+        self._fetch_blocks()
+
+    def _fetch_blocks(self):
+        """Fetch the code blocks from the language section using requests and BeautifulSoup."""
+        url = f"https://rosettacode.org/wiki/{self.task_name}#{self.name}"
+        logging.info(f"Fetching code blocks from {url}")
+        response = requests.get(url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, "lxml")
+            language_section = soup.find("span", id=self.name).parent
+            if language_section:
+                code_tags = language_section.find_all("pre")
+                for tag in code_tags:
+                    block = CodeBlock(tag)
+                    self.blocks.append(block)
+            else:
+                logging.warning(
+                    f"No code blocks found for language {self.name} in task {self.task_name}"
+                )
+        else:
+            logging.error(
+                f"Failed to fetch code blocks from {url}: {response.status_code}"
+            )
 
     def to_json(self):
+        """Return a JSON-serializable representation of the language."""
         return {"name": self.name, "blocks": [block.to_json() for block in self.blocks]}
 
 
 class CodeBlock:
-    def __init__(self, code, output=None, metadata=None):
-        self.code = code
-        self.output = output
-        self.metadata = metadata
+    """A class that represents a code block on a language section on Rosetta Code."""
 
-    def to_json(self):
-        return {"code": self.code, "output": self.output, "metadata": self.metadata}
+    def __init__(self, tag):
+        """Initialize the code block with a BeautifulSoup tag and extract its attributes."""
+        self.tag = tag
+        self.code = tag.text
+        self.output = None
+        self.metadata = {}
+        self._extract_attributes()
+
+    def _extract_attributes(self):
+        """Extract the attributes of the code block from its tag."""
+        # check if the tag has a class attribute
+        if self.tag.has_attr("class"):
+            # get the value of the class attribute as a list
+            classes = self.tag["class"]
+            # loop through the classes
+            for cls in classes:
+                # check if the class starts with "output-"
+                if cls.startswith("output-"):
+                    # get the output type after the dash
+                    output_type = cls.split("-")[1]
+                    # set the output attribute to the output type
+                    self.output = output_type
+                # check if the class is "works-with"
+                elif cls == "works-with":
+                    # get the next sibling tag, which should be a span tag
+                    span_tag = self.tag.next_sibling
+                    # check if the span tag has a title attribute
+                    if span_tag and span_tag.has_attr("title"):
+                        # get the value of the title attribute as a string
+                        works_with = span_tag["title"]
+                        # set the metadata attribute with the key "works-with" and the value as a list of works-with items
+                        self.metadata["works-with"] = works_with.split(", ")
+                # check if the class is "lang"
+                elif cls == "lang":
+                    # get the next sibling tag, which should be a span tag
+                    span_tag = self.tag.next_sibling
+                    # check if the span tag has a text attribute
+                    if span_tag and span_tag.text:
+                        # get the value of the text attribute as a string
+                        lang = span_tag.text
+                        # set the metadata attribute with the key "lang" and the value as a list of lang items
+                        self.metadata["lang"] = lang.split(", ")
