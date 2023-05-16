7cdb03476ce98f613654b027830e4d1ae861f27d
AlessandroHultman <alessandrosisfontes@hultman.com>
2023-05-16 12:04:39 +0200

    Add option to scrape all code from rosetta without pagination handling.

--- rccli.py
+++ rccli.py
@@ -1,50 +1,100 @@
-from progress_bar import progress_bar
-
 import requests
 from bs4 import BeautifulSoup
 import argparse
 import os
+from progress_bar import progress_bar
 
-# Define the command line arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('--task', help='The name of the task to scrape')
+parser.add_argument("--task", help="Scrape a specific task by name")
+parser.add_argument("--output", default="/Users/ash/Desktop/rosettacode/code", help="Output directory for scraped tasks")
 args = parser.parse_args()
 
-# Check if the task argument is given
-if args.task:
-    # Construct the URL of the task page
-    url = f'https://rosettacode.org/wiki/{args.task}'
-    # Make a request to the URL and get the HTML content
+output_dir = args.output
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+def scrape_task(task_name):
+    url = f"https://rosettacode.org/wiki/{task_name}"
     response = requests.get(url)
-    # Parse the HTML content using BeautifulSoup
-    soup = BeautifulSoup(response.text, 'html.parser')
-    # Find all the <pre> tags that contain the source code
-    pre_tags = soup.find_all('pre')
-    index = 0;
-    progress_bar(index, len(pre_tags))
-    # Loop through each <pre> tag
+    soup = BeautifulSoup(response.text, "html.parser")
+    pre_tags = soup.find_all("pre")
+
+    supported_languages = {
+        "c", "cpp", "objc", "swift", "d", "rust", "fortran", "ada", "haskell", "kotlin", "julia"
+    }
+
     for pre_tag in pre_tags:
-        # Get the name of the programming language from the previous <h2> tag
-        lang = pre_tag.find_previous('h2').text.strip()
-        # Remove the "[edit]" part from the language name
-        lang = lang.replace('[edit]', '')
-        # Escape the backslashes in the language name
-        lang = lang.replace('\\', '\\\\')
-        # Replace the slashes in the language name with dashes
-        lang = lang.replace('/', '-')
-        # Get the source code from the <pre> tag
+        h2_tag = pre_tag.find_previous("h2")
+        if h2_tag is None:
+            continue
+
+        lang = h2_tag.text.strip()
+        lang = lang.replace("[edit]", "")
+        lang = lang.replace("\\", "\\\\")
+        lang = lang.replace("/", "-")
         code = pre_tag.text.strip()
-        # Create a directory named 'code' if it does not exist
-        if not os.path.exists(args.task):
-            os.mkdir(args.task)
-        # Create a file named '<language>.txt' in the 'code' directory
-        file_name = f'{lang}.txt'
-        file_path = os.path.join(args.task, file_name)
-        # Write the source code to the file
-        with open(file_path, 'w') as f:
-            f.write(code)
-        index = index + 1
-        progress_bar(index, len(pre_tags))
+
+        if lang.lower() not in supported_languages:
+            continue
+
+        lang_dir = os.path.join(output_dir, lang)
+        os.makedirs(lang_dir, exist_ok=True)
+
+        file_name = f"{task_name}.txt"
+        file_path = os.path.join(lang_dir, file_name)
+
+        try:
+            # Create parent directories recursively if they don't exist
+            os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+            with open(file_path, "w") as f:
+                f.write(code)
+        except OSError as e:
+            print(f"Failed to write file: {file_path}. Error: {str(e)}")
+
+
+def scrape_rosetta_code():
+    base_url = "https://rosettacode.org"
+    tasks_url = base_url + "/wiki/Category:Programming_Tasks"
+
+    response = requests.get(tasks_url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    page_links = soup.find_all("a", class_="mw-category-page-link")
+
+    for page_link in page_links:
+        page_url = base_url + page_link["href"]
+        page_response = requests.get(page_url)
+        page_soup = BeautifulSoup(page_response.text, "html.parser")
+        task_links = page_soup.find_all("a", class_="mw-category")
+
+        for task_link in task_links:
+            task_name = task_link.text.strip()
+            scrape_task(task_name)
+
+
+# Scrape a specific task if provided
+if args.task:
+    task_name = args.task
+    scrape_task(task_name)
+    print(f"Scraped task: {task_name}")
 else:
-    # Print a message indicating the task argument is missing
-    print('Please provide a task name using --task argument')
+    # Scrape all tasks
+    print("Scraping tasks from Rosetta Code...")
+
+    # Get the list of all tasks
+    url = "https://rosettacode.org/wiki/Category:Programming_Tasks"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    task_links = soup.select("#mw-pages .mw-category-group ul li a")
+
+    total_tasks = len(task_links)
+    current_task = 0
+
+    progress_bar(0, total_tasks)
+    for task_link in task_links:
+        task_name = task_link.text.strip()
+        scrape_task(task_name)
+        current_task += 1
+        progress_bar(current_task, total_tasks)
+
+    print("Scraping completed.")
